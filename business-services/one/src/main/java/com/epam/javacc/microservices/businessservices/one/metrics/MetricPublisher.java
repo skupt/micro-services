79182b7e7e0571f3db31a95af910416e1b1de27d
Vitalii_Rozaronov <v.vonera@gmail.com>
2022-07-31 16:50:26 +0300

    Graphite and Zipkin are added

--- business-services/one/src/main/java/com/epam/javacc/microservices/businessservices/one/metrics/MetricPublisher.java
+++ business-services/one/src/main/java/com/epam/javacc/microservices/businessservices/one/metrics/MetricPublisher.java
@@ -1,14 +1,8 @@
 package com.epam.javacc.microservices.businessservices.one.metrics;
 
-import com.netflix.servo.publish.BasicMetricFilter;
-import com.netflix.servo.publish.MonitorRegistryMetricPoller;
-import com.netflix.servo.publish.PollRunnable;
-import com.netflix.servo.publish.PollScheduler;
-import com.netflix.servo.publish.atlas.AtlasMetricObserver;
-import com.netflix.servo.publish.atlas.BasicAtlasConfig;
-import com.netflix.servo.publish.atlas.ServoAtlasConfig;
-import com.netflix.servo.tag.BasicTagList;
-import com.netflix.servo.tag.TagList;
+import com.netflix.servo.publish.*;
+import com.netflix.servo.publish.graphite.GraphiteMetricObserver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -17,17 +11,18 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Component
 public class MetricPublisher {
+    @Value("${METRIC_OBSERVER_URI}")
+    private String observerUri = "localhost";
+
     @PostConstruct
     private void init() {
         System.setProperty("servo.pollers", "1000");
-        System.setProperty("servo.atlas.batchSize", "1");
-        System.setProperty("servo.atlas.uri", "http://localhost:7101/api/v1/publish");
-        ServoAtlasConfig config = new BasicAtlasConfig();
-        TagList commonTags = BasicTagList.of("servo", "counter", "servo", "one_counter_01", "servo", "one_timer_01");
-        AtlasMetricObserver observer = new AtlasMetricObserver(config, commonTags);
+        String prefix = "one";
+        String addr = observerUri + ":2003";
+        MetricObserver observer = new GraphiteMetricObserver(prefix, addr);
         PollRunnable pollRunnable = new PollRunnable(new MonitorRegistryMetricPoller(),
                 new BasicMetricFilter(true), observer);
         PollScheduler.getInstance().start();
-        PollScheduler.getInstance().addPoller(pollRunnable, 1, SECONDS);
+        PollScheduler.getInstance().addPoller(pollRunnable, 3, SECONDS);
     }
 }
